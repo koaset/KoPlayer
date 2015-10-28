a8f5ad65a1bbbd61a1f8f9ba233b20c3289d0625
Karl-Oskar Smed <karloskarsmed89@gmail.com>
2015-10-28 15:39:58 +0100

    Restructured source files

--- bin/Playlists/Shuffle Queue.pl
+++ bin/Playlists/Shuffle Queue.pl
@@ -1,6 +1,23 @@
 <?xml version="1.0"?>
 <Playlist xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-  <songPaths />
+  <songPaths>
+    <string>D:\Musik\=NeutraL=\Darkness\10. nazz-can - Òé¿ÒâìÒâ½Òé«Òâ+Ú+ÄµÿÄ ´¢× Future Dream.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\08. nazz-can - Òâ®Òé¦ÒâêÒâ¬ÒâóÒâ+Òâê.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\03. nazz-can - þÀæþ£+Òü«Òé©ÒéºÒâ®ÒéÀÒâ+.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\09. nazz-can - ÒâÅÒâ½ÒâêÒâ×Òâ¦Òü«ÕªûµÇ¬Õ¦æÕÑ¦.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\06. nazz-can - µ¡+õ¢ôµùàÞíî ´¢× Be of good cheer!.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\06. nazz-can - µ¡+õ¢ôµùàÞíî ´¢× Be of good cheer!.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\02. nazz-can - Õ¦üÒüÿÒéëÒéîÒüƒÕªûµÇ¬ ´¢× Lost Place.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\01. nazz-can - Õ£¦Ú£èÚüöÒü«ÞÁÀÕ¦è.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\03. nazz-can - þÀæþ£+Òü«Òé©ÒéºÒâ®ÒéÀÒâ+.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\09. nazz-can - ÒâÅÒâ½ÒâêÒâ×Òâ¦Òü«ÕªûµÇ¬Õ¦æÕÑ¦.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\09. nazz-can - ÒâÅÒâ½ÒâêÒâ×Òâ¦Òü«ÕªûµÇ¬Õ¦æÕÑ¦.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\07. nazz-can - Ú£èþƒÑÒü«Õñ¬ÚÖ¢õ+íõ+¦ ´¢× Nuclear Fusion.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\08. nazz-can - Òâ®Òé¦ÒâêÒâ¬ÒâóÒâ+Òâê.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\05. nazz-can - Õ¦æÕÑ¦ÒüòÒü¿Òéè ´¢× 3rd eye.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\01. nazz-can - Õ£¦Ú£èÚüöÒü«ÞÁÀÕ¦è.mp3</string>
+    <string>D:\Musik\=NeutraL=\Darkness\06. nazz-can - µ¡+õ¢ôµùàÞíî ´¢× Be of good cheer!.mp3</string>
+  </songPaths>
   <Name>Shuffle Queue</Name>
   <CurrentIndex>0</CurrentIndex>
 </Playlist>
