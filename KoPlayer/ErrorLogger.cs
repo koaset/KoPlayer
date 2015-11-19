1cc2b9afa6ce52a761ccbda9fd22248fcbbd2f63
Karl-Oskar Smed <karloskarsmed89@gmail.com>
2015-11-19 16:10:19 +0100

    Added logging of unhandled exceptions

---
+++ KoPlayer/ErrorLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KoPlayer
+{
+    /// <summary>
+    /// Writes an exception to a log file
+    /// </summary>
+    class ErrorLogger : IDisposable
+    {
+        private Exception ex;
+        private StreamWriter sw;
+        private const string logPath = "error.log";
+
+        public ErrorLogger(Exception ex)
+        {
+            this.ex = ex;
+        }
+
+        /// <summary>
+        /// Saves exception info to log file
+        /// </summary>
+        public void Run()
+        {
+            using (sw = new StreamWriter(logPath, true))
+            {
+                sw.WriteLine(DateTime.Now.ToString());
+                sw.WriteLine("Error: " + ex.ToString());
+                sw.WriteLine();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (sw != null)
+                sw.Dispose();
+        }
+    }
+}
--- KoPlayer/Program.cs
+++ KoPlayer/Program.cs
@@ -13,7 +13,29 @@ namespace KoPlayer
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "-nolog")
+            {
+                Run();
+            }
+            else
+            {
+                try
+                {
+                    Run();
+                }
+                catch (Exception e)
+                {
+                    using (var errorLogger = new ErrorLogger(e))
+                    {
+                        errorLogger.Run();
+                    }
+                }
+            }
+        }
+
+        static void Run()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
