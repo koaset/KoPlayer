5f46723972fb52316b2ee791be95a952bbd2ec41
Karl-Oskar Smed <karloskarsmed89@gmail.com>
2015-12-29 16:04:19 +0100

    Refactored tray icon fix code

--- KoPlayer/Forms/MainForm.cs
+++ KoPlayer/Forms/MainForm.cs
@@ -638,7 +638,22 @@ namespace KoPlayer.Forms
             }
             else
                 text = this.Text;
-            Fixes.SetNotifyIconText(trayIcon, text);
+            SetNotifyIconText(trayIcon, text);
+        }
+
+        /// <summary>
+        /// Ups limit on tray icon text from 63 to 127 chars. Found at stackoverflow
+        /// </summary>
+        /// <param name="ni"></param>
+        /// <param name="text"></param>
+        public static void SetNotifyIconText(NotifyIcon ni, string text)
+        {
+            if (text.Length >= 128) throw new ArgumentOutOfRangeException("Text limited to 127 characters");
+            Type t = typeof(NotifyIcon);
+            BindingFlags hidden = BindingFlags.NonPublic | BindingFlags.Instance;
+            t.GetField("text", hidden).SetValue(ni, text);
+            if ((bool)t.GetField("added", hidden).GetValue(ni))
+                t.GetMethod("UpdateIcon", hidden).Invoke(ni, new object[] { true });
         }
 
         private void SetTrayIconContextMenu()
@@ -1838,6 +1853,9 @@ namespace KoPlayer.Forms
 
             foreach (DataGridViewRow row in sortedRows)
                 songs.Add((Song)row.DataBoundItem);
+
+            songs.Reverse();
+
             shuffleQueue.Insert(shuffleQueue.CurrentIndex + 1, songs);
 
             if (showingPlaylist == shuffleQueue)
